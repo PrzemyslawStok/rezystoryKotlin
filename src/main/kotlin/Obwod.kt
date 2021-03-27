a16923d736384c5b524dad734a072a7fd81a4a17
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2021-03-27 14:14:28 +0100

    cd..

--- src/main/kotlin/Obwod.kt
+++ src/main/kotlin/Obwod.kt
@@ -1,9 +1,17 @@
-class Obwod(val typ: Typ) {
+class Obwod(val typ: Typ, val R1: Double, val R2: Double) {
     enum class Typ{
         SZEREGOWY, ROWNOLEGLY
     }
 
     fun print(){
         println("obwód: ${typ.name}")
+    }
+
+    fun oblicz():Double{
+        return obliczSzeregowy()
+    }
+
+    fun obliczSzeregowy():Double{
+        return 0.0
     }
 }
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,4 +1,4 @@
 fun main() {
-    val obwod1 = Obwod(Obwod.Typ.SZEREGOWY)
+    val obwod1 = Obwod(Obwod.Typ.SZEREGOWY, 1.0, 1.0)
     obwod1.print()
 }
