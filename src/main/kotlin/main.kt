c8149e7a95c86512ba4655807851685bb392e726
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2021-03-27 15:17:13 +0100

    cd..

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,14 +1,23 @@
 fun main() {
-    val obwod1 = Obwod(Obwod.Typ.SZEREGOWY,2.0)
+    val obwod1 = Obwod(Obwod.Typ.ROWNOLEGLY,1.0)
     val obwod2 = Obwod(1.0)
+    val obwod3 = Obwod(2.0)
 
+    val obwod4 = Obwod(0.0)
+    val obwod5 = Obwod(5.0)
+
+    obwod4.dodajObwod(obwod2)
+    obwod4.dodajObwod(obwod3)
+
+    obwod1.dodajObwod(obwod4)
     obwod1.dodajObwod(obwod2)
     obwod1.dodajObwod(obwod2)
     obwod1.dodajObwod(obwod2)
-    obwod1.dodajObwod(obwod2)
-    obwod1.dodajObwod(obwod2)
+    obwod1.dodajObwod(obwod5)
 
     obwod1.print()
+
+
 
     //obwod2.print()
 }
